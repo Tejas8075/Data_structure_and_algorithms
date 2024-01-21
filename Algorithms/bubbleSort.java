b54c17f7186beceaae1613ae1824452a8ae020b0
Tejas8075 <tejasmahangade61@gmail.com>
2024-01-21 16:58:30 +0530

    Add bubble sort

---
+++ Algorithms/bubbleSort.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class bubbleSort{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int b[] = new int[n];
+        int temp = 0;
+        
+    }
+}
